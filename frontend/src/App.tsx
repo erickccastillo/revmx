d1b3d3a64066f52d84d98ff20fa95f84857cee19
Erick <erick.castillo5400@alumnos.udg.mx>
2026-08-17 20:54:32 -0600

    Admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
+// 1. Importa tu componente del formulario administrador
+import AdminProductForm from './pages/AdminProductForm'; 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Admin from './pages/AdminProductForm';
+
 import './App.css';
 
 const App: React.FC = () => {
@@ -15,7 +17,9 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/catalog" element={<Catalog />} />
-     
+          
+          {/* 2. Agrega la nueva ruta para el panel */}
+          <Route path="/admin" element={<AdminProductForm />} />
           
         </Routes>
       </main>
