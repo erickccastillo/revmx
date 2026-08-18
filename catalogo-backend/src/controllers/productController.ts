dca4bd4ec32d7a3807ecd977e218096eca189a3c
erickccastillo <erick.ccastillo54@gmail.com>
2026-08-18 07:39:13 -0600

    Update productController.ts

--- catalogo-backend/src/controllers/productController.ts
+++ catalogo-backend/src/controllers/productController.ts
@@ -19,14 +19,35 @@ export const getProducts = async (req: Request, res: Response) => {
 // Función para crear productos (Panel de Administrador)
 export const createProduct = async (req: Request, res: Response) => {
   try {
-    const { name, description, price, category, image_url } = req.body;
+    const {
+      name,
+      description,
+      price,
+      category,
+      image_url,
+      color,
+      material,
+      medidas
+    } = req.body;
 
     const { data, error } = await supabase
       .from('products')
-      .insert([{ name, description, price, category, image_url }])
+      .insert([
+        {
+          name,
+          description,
+          price,
+          category,
+          image_url,
+          color,
+          material,
+          medidas
+        }
+      ])
       .select();
 
     if (error) throw error;
+
     res.status(201).json(data);
   } catch (error) {
     res.status(500).json({ error: 'Error al crear producto' });
@@ -34,18 +55,41 @@ export const createProduct = async (req: Request, res: Response) => {
 };
 
 export const updateProduct = async (req: Request, res: Response) => {
-  const { id } = req.params; // Obtenemos el ID de la URL
-  const { name, description, price, category, image_url } = req.body;
+  const { id } = req.params;
+
+  const {
+    name,
+    description,
+    price,
+    category,
+    image_url,
+    color,
+    material,
+    medidas
+  } = req.body;
 
   try {
     const { data, error } = await supabase
       .from('products')
-      .update({ name, description, price, category, image_url })
+      .update({
+        name,
+        description,
+        price,
+        category,
+        image_url,
+        color,
+        material,
+        medidas
+      })
       .eq('id', id)
       .select();
 
     if (error) throw error;
-    res.status(200).json({ message: 'Producto actualizado con éxito', product: data[0] });
+
+    res.status(200).json({
+      message: 'Producto actualizado con éxito',
+      product: data[0]
+    });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
