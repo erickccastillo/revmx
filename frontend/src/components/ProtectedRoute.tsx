e3932326821023668a31eb4f7239d4330b20b261
erickccastillo <erick.ccastillo54@gmail.com>
2026-08-19 09:20:10 -0600

    Create ProtectedRoute.tsx

---
+++ frontend/src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+// src/components/ProtectedRoute.tsx
+import { Navigate } from "react-router-dom";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ProtectedRoute({ children }: Props) {
+  const token = localStorage.getItem("adminToken");
+
+  if (!token) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <>{children}</>;
+}
