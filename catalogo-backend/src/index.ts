f10d644b701e16b412a7c3fcd1a4a9837d3c9433
erickccastillo <erick.ccastillo54@gmail.com>
2026-08-19 09:16:28 -0600

    Add authentication routes to the server

--- catalogo-backend/src/index.ts
+++ catalogo-backend/src/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import productRoutes from './routes/productRoutes';
-
+import authRoutes from "./routes/auth"
 dotenv.config();
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
-
+app.use("/auth", authRoutes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`✅ Servidor backend corriendo en el puerto ${PORT}`);
