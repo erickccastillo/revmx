de1e5cde44b8c2ed095e663bbe1e547fabb62de0
erickccastillo <erick.ccastillo54@gmail.com>
2026-08-19 10:37:26 -0600

    Update ProtectedRoute.tsx

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-type Props = {
+export default function ProtectedRoute({
+  children,
+}: {
   children: React.ReactNode;
-};
-
-export default function ProtectedRoute({ children }: Props) {
+}) {
   const token = localStorage.getItem("adminToken");
 
   if (!token) {
