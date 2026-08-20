97d84119c943262f6ed42013034d52f975c41638
erickccastillo <erick.ccastillo54@gmail.com>
2026-08-20 10:38:06 -0600

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -19,19 +19,30 @@ export type ProductsResponse = {
 };
 
 
-export const getProducts = async (params: { q?: string; category?: string; page?: number; limit?: number }) => {
+export const getProducts = async (params: {
+  q?: string;
+  category?: string;
+  page?: number;
+  limit?: number;
+}) => {
   const query = new URLSearchParams();
+
   if (params.q) query.append('search', params.q);
-  if (params.category && params.category !== 'TODOS') query.append('category', params.category);
+  if (params.category && params.category !== 'TODOS')
+    query.append('category', params.category);
   if (params.page) query.append('page', String(params.page));
   if (params.limit) query.append('limit', String(params.limit));
 
-  const res = await fetch(`http://localhost:4000/products?${query.toString()}`);
+  const res = await fetch(
+    `${baseURL}/api/products?${query.toString()}`
+  );
+
   return res.json();
 };
 
+
 export const getFeatured = async () => {
-  const res = await fetch('http://localhost:4000/featured');
+  const res = await fetch(`${baseURL}/api/featured`);
   return res.json();
 };
 
